ea0be57a3b6085e1569e5fb6dacf23a157e6263a
Ibrahnab <ibrahim.naboulsi97@gmail.com>
2025-08-07 03:33:04 +0200

    Add signout button in home view

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>TBD</main>
+  <div>
+    <SpinnerButton :spinner="showSpinnerSignout" @click="signOut">Sign Out</SpinnerButton>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { TextBox, SpinnerButton, ErrorBox } from '@/components/common'
+import { ref } from 'vue'
+import { useUserStore } from '@/stores/userStore'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const showSpinnerSignout = ref(false)
+
+async function signOut() {
+  try {
+    localStorage.clear()
+    router.push('/')
+  } catch (error) {}
+}
+</script>
